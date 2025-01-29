91ac12e2adcbd97c275be69e0f0f8d860419db3a
hooved <172129504+hooved@users.noreply.github.com>
2025-01-29 13:29:06 -0500

    query max cache/gpuBuf

--- index.js
+++ index.js
@@ -524,6 +524,12 @@ document.addEventListener("alpine:init", () => {
           console.log(`error: ${error}\nFailed to launch WebGPU. Loading WASM model instead...`); // return;
         }
       }
+      const storageEstimate = await navigator.storage.estimate();
+      try {
+        var maxGpuBuffer = device.limits.maxBufferSize;
+      } catch (error) {var maxGpuBuffer = null;}
+      this.progress(0,100, `avail storage: ${storageEstimate}, maxgpubuffer: ${maxGpuBuffer}`);
+      return;
 
       try {
         var tensorData = await getAndDecompressGGUFChunks(device, this.progress.bind(this));
